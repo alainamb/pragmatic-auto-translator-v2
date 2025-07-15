90dd16570fca8440b6abe9438641528a4be2d004
AlliBatell <alb594@pitt.edu>
2025-07-15 13:31:14 -0400

    env variables test

--- scripts/mongodb.py
+++ scripts/mongodb.py
@@ -2,7 +2,7 @@ from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 
 #sub in db_username and db_passcode, for vercel or modal specifics depending
-uri = "mongodb+srv://<db_username>:<db_password>@pragmatic-auto-translat.grvvg7v.mongodb.net/?retryWrites=true&w=majority&appName=Pragmatic-Auto-Translator"
+uri = process.env.MongoDB_URI
 
 # Create a new client and connect to the server
 client = MongoClient(uri, server_api=ServerApi('1'))
